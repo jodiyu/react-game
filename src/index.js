741e792686631f203e2090cd7991084fb5019fdf
Jodi Yu <jodiyu@bu.edu>
2025-01-14 17:08:41 -0800

    maybe the gh pages was the friends we made along the way

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
 //import BrowserRouter from 'react-router-dom/BrowserRouter';
 
 
@@ -11,9 +11,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    {/* <BrowserRouter basename={process.env.PUBLIC_URL}> */}
       <App />
-    </BrowserRouter>
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
 
