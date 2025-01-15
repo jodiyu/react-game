bceed72430e593077b36d7ce2ea570a431fe4f11
Jodi Yu <jodiyu@bu.edu>
2025-01-14 17:04:10 -0800

    at this point

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import TicTacToe from './TicTacToe';
 // import TicTacToe from './Components/TicTacToe/TicTacToe';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+
+// function App() {
+//   return (
+//     <div>
+//       <TicTacToe/>
+//     </div>
+//   );
+// }
 function App() {
   return (
-    <div>
-      <TicTacToe/>
-    </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<TicTacToe />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
